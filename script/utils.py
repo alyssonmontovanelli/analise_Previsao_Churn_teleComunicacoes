6520e39584de975d1a0875346347ac31b6686028
Alysson Montovanelli <alysson.montovanelli@gmail.com>
2023-08-16 16:53:01 -0300

    final eng atributos - start pre processamento

---
+++ script/utils.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
